84172047f66fbc7cbb6172bba4215569272520ec
Guillaume MANCIET <mudounet@free.fr>
2011-09-01 15:32:49 +0000

    git-svn-id: http://manciet.unfuddle.com/svn/manciet_java/MovieManager/trunk@55 b959ae9e-4125-4918-a975-fad86ba83c5c

--- src/test/java/com/mudounet/util/managers/SimpleTagManagerTest.java
+++ src/test/java/com/mudounet/util/managers/SimpleTagManagerTest.java
@@ -4,25 +4,32 @@
  */
 package com.mudounet.util.managers;
 
+import com.mudounet.hibernate.movie.Movie;
 import com.mudounet.test.ResultSetReporter;
 import org.dbunit.dataset.ITable;
 import org.hibernate.Transaction;
 import org.hibernate.Session;
 import com.mudounet.utils.dbunit.ProjectDatabaseTestCase;
 import com.mudounet.hibernate.tags.GenericTag;
+import com.mudounet.util.hibernate.HibernateFactory;
 import java.util.ArrayList;
+import java.util.List;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
 
 /**
  *
  * @author gmanciet
  */
 public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
+
+    protected static Logger logger = Logger.getLogger(SimpleTagManagerTest.class.getName());
     private Session session;
     private Transaction tx;
-    
+
     public SimpleTagManagerTest(String name) {
         super(name);
     }
@@ -30,8 +37,8 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
     @Override
     protected DatabaseOperation getTearDownOperation() {
         return DatabaseOperation.NONE;
-    }    
-    
+    }
+
     /**
      * Test of addFilterTag method, of class TagManager.
      */
@@ -54,18 +61,41 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
         SimpleTagManager instance = new SimpleTagManager();
         ArrayList expResult = null;
         ArrayList result = instance.getTagLists();
-        
+
         ITable resultSet //= this.getResults("select * from GENERICMOVIE AS G,GENERICTAG AS T");
-        = this.getResults("select TITLE from GENERICMOVIE AS M, GENERICTAG as T, MOVIES_TAGS as MT where M.ID = MT.FK_MOVIE and T.ID = MT.FK_TAG AND (T.KEY='Comedie')");
-        logger.debug("Number of films: "+resultSet.getRowCount());
-       
-        ResultSetReporter.dump(resultSet);     
-        
+                = this.getResults("select TITLE from GENERICMOVIE AS M, GENERICTAG as T, MOVIES_TAGS as MT where M.ID = MT.FK_MOVIE and T.ID = MT.FK_TAG AND (T.KEY='Comedie')");
+        logger.debug("Number of films: " + resultSet.getRowCount());
+
+        session = HibernateFactory.openSession();
+
+        String[] tags = {"Oscar", "Documentaire", "Comedie"};
+
+        String hql = "select m from Movie m "
+                + "join m.tags t "
+                + "where t.key in (:tags) "
+                + "group by m "
+                + "having count(t)=:tag_count";
+
+        Query query = session.createQuery(hql);
+        query.setParameterList("tags", tags);
+        query.setInteger("tag_count", tags.length);
+        List<Movie> results = query.list();
+
+        for (int i = 0; i < results.size(); i++) {
+            logger.debug(results.get(i));
+        }
+
+        session.close();
+
+        logger.debug("Results : " + results.size());
+
+        //ResultSetReporter.dump(resultSet);     
+
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
     protected String getDataSetFilename() {
         return "TestSimpleTagManager.xml";
     }
