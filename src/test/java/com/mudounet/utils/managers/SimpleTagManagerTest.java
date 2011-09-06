7ee0d5ca09ead850c49c7100c5ad612a5aad214a
Guillaume MANCIET <mudounet@free.fr>
2011-09-06 21:44:58 +0000

    Continued Test case.
    git-svn-id: http://manciet.unfuddle.com/svn/manciet_java/MovieManager/trunk@74 b959ae9e-4125-4918-a975-fad86ba83c5c

--- src/test/java/com/mudounet/utils/managers/SimpleTagManagerTest.java
+++ src/test/java/com/mudounet/utils/managers/SimpleTagManagerTest.java
@@ -50,7 +50,7 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
 
         assertEquals(1, instance.getFilterTagsList().size());
     }
-    
+
     /**
      * Test of getFilterTagsList method, of class SimpleTagManager.
      */
@@ -58,13 +58,13 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
     public void testGetFilterTagsList() throws Exception {
         logger.info("getFilterTagsList");
         SimpleTagManager instance = new SimpleTagManager();
-        
+
         ITable resultSet = this.getResults("select KEY FROM GENERICTAG  WHERE TYPE='S'");
 
         for (int i = 0; i < resultSet.getRowCount(); i++) {
             instance.addFilterTag((String) resultSet.getValue(i, "key"));
-            
-            assertEquals(i+1, instance.getFilterTagsList().size());
+
+            assertEquals(i + 1, instance.getFilterTagsList().size());
         }
     }
 
@@ -74,15 +74,57 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
     @Test
     public void testGetTagLists() throws Exception {
         logger.info("getTagLists");
-        SimpleTagManager instance = new SimpleTagManager();
-        
-        instance.addFilterTag("Oscar");
-        
-        
-        
-       instance.getTagLists();
 
-        
+        // Building list of tags to test
+        ITable resultSet = this.getResults("select KEY FROM GENERICTAG  WHERE TYPE='S'");
+        ArrayList<String> completeTagList = new ArrayList<String>();
+        for (int i = 0; i < resultSet.getRowCount(); i++) {
+            completeTagList.add((String) resultSet.getValue(i, "key"));
+        }
+        Collections.shuffle(completeTagList);
+
+        ArrayList<String> testedTagList = new ArrayList<String>();
+        String keyList = "";
+        for (String key : completeTagList) {
+            // Testing progressively tags
+            testedTagList.add(key);
+
+            // Building list of tags used by SQL query
+            if (keyList.equals("")) {
+                keyList = "'" + key + "'";
+            } else {
+                keyList = keyList + ", '" + key + "'";
+            }
+            logger.debug("Testing keys : " + keyList);
+
+            // Building list of tags used by Hibernate 
+            SimpleTagManager st = new SimpleTagManager();
+            for (String testedKey : testedTagList) {
+                st.addFilterTag(testedKey);
+            }
+
+            String query = "SELECT KEY, COUNT(*) AS FILMS_COUNT FROM GENERICTAG as T, MOVIES_TAGS as MT  WHERE T.TYPE='S' AND T.ID = MT.FK_TAG AND MT.FK_MOVIE IN ( "
+                    + "select M.ID from GENERICMOVIE AS M, GENERICTAG as T, MOVIES_TAGS as MT where M.ID = MT.FK_MOVIE and T.ID = MT.FK_TAG AND (T.KEY = 'Animation') AND FK_MOVIE IN (SELECT Movies_Tags.fk_movie "
+                    + "FROM Movies_Tags "
+                    + "INNER JOIN GenericMovie a "
+                    + "ON a.id = Movies_Tags.fk_movie "
+                    + "INNER JOIN Generictag t "
+                    + "ON t.id = Movies_Tags.fk_tag "
+                    + "WHERE t.key IN ("+keyList+") "
+                    + "GROUP BY Movies_Tags.fk_movie "
+                    + "HAVING Count(Movies_Tags.fk_tag) = "+testedTagList.size()+")) AND KEY NOT IN ("+keyList+") "
+                    + "GROUP BY KEY";
+
+            resultSet = this.getResults(query);
+            List<TagResult> tagList = st.getTagLists();
+
+            assertEquals("Items returned did not match : ", resultSet.getRowCount(), tagList.size());
+
+
+            for (TagResult tag : tagList) {
+            }
+        }
+
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -90,8 +132,6 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
     protected String getDataSetFilename() {
         return "TestSimpleTagManager.xml";
     }
-
-
 
     /**
      * Test of getMovies method, of class SimpleTagManager.
@@ -109,33 +149,36 @@ public class SimpleTagManagerTest extends ProjectDatabaseTestCase {
         }
         Collections.shuffle(completeTagList);
 
-        
+
         ArrayList<String> testedTagList = new ArrayList<String>();
         String keyList = "";
         for (String key : completeTagList) {
             testedTagList.add(key);
-            
-            if(keyList.equals("")) keyList = "'" + key+ "'";
-            else keyList = keyList + ", '" + key+ "'";
+
+            if (keyList.equals("")) {
+                keyList = "'" + key + "'";
+            } else {
+                keyList = keyList + ", '" + key + "'";
+            }
 
             logger.debug("Testing keys : " + keyList);
-            
+
             SimpleTagManager st = new SimpleTagManager();
             for (String testedKey : testedTagList) {
                 st.addFilterTag(testedKey);
             }
             List<GenericMovie> result = st.getMovies();
-            
-            String query = "SELECT fk_movie " +
- "FROM Movies_Tags INNER JOIN GenericMovie m ON m.id = fk_movie INNER JOIN Generictag t ON t.id = fk_tag " +
-" WHERE t.key IN (" + keyList + ") "+
-"GROUP BY fk_movie " +
-"HAVING Count(fk_tag) = " + testedTagList.size();
-            
+
+            String query = "SELECT fk_movie "
+                    + "FROM Movies_Tags INNER JOIN GenericMovie m ON m.id = fk_movie INNER JOIN Generictag t ON t.id = fk_tag "
+                    + " WHERE t.key IN (" + keyList + ") "
+                    + "GROUP BY fk_movie "
+                    + "HAVING Count(fk_tag) = " + testedTagList.size();
+
             resultSet = this.getResults(query);
-            
+
             logger.debug("Number of films found : " + resultSet.getRowCount());
-            
+
             assertEquals("Number of films for key(s) " + keyList + " : ", resultSet.getRowCount(), result.size());
         }
     }
