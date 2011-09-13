a1db8705aba9bb647465f29d1d2574106df2d188
Guillaume MANCIET <mudounet@free.fr>
2011-09-13 19:47:42 +0000

    Moved files to correct directories.
    git-svn-id: http://manciet.unfuddle.com/svn/manciet_java/MovieManager/trunk@92 b959ae9e-4125-4918-a975-fad86ba83c5c

---
+++ src/main/java/com/mudounet/utils/managers/MovieManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet.utils.managers;
+
+import com.mudounet.hibernate.movies.GenericMovie;
+import com.mudounet.hibernate.tags.GenericTag;
+
+/**
+ *
+ * @author isabelle
+ */
+public class MovieManager {
+    
+    public static boolean addMovie(String path, String title) {
+        return false;
+    }
+    
+    public static boolean addTagToMovie(GenericMovie movie, GenericTag tag) {
+        
+    }
+}
