fbeb2729b095ee3651a423d027d5a9a96ac79d57
Guillaume MANCIET <mudounet@free.fr>
2011-09-15 10:47:40 +0000

    Handling of OS flavours.
    git-svn-id: http://manciet.unfuddle.com/svn/manciet_java/MovieManager/trunk@118 b959ae9e-4125-4918-a975-fad86ba83c5c

--- src/main/java/com/mudounet/utils/managers/MovieToolManager.java
+++ src/main/java/com/mudounet/utils/managers/MovieToolManager.java
@@ -22,40 +22,66 @@ import uk.co.caprica.vlcj.player.events.VideoOutputEventListener;
 public class MovieToolManager {
 
     protected static Logger logger = Logger.getLogger(SimpleTagManager.class.getName());
-    
+    private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
+
+    /**
+     * Test whether the runtime operating system is "unix-like".
+     * 
+     * @return true if the runtime OS is unix-like, Linux, Unix, FreeBSD etc
+     */
+    public static boolean isNix() {
+        return OS_NAME.indexOf("nux") != -1 || OS_NAME.indexOf("nix") != -1 || OS_NAME.indexOf("freebsd") != -1;
+    }
+
+    /**
+     * Test whether the runtime operating system is a Windows variant. 
+     * 
+     * @return true if the runtime OS is Windows
+     */
+    public static boolean isWindows() {
+        return OS_NAME.indexOf("win") != -1;
+    }
+
+    /**
+     * Test whether the runtime operating system is a Mac variant.
+     * 
+     * @return true if the runtime OS is Mac
+     */
+    public static boolean isMac() {
+        return OS_NAME.indexOf("mac") != -1;
+    }
+
     public static TechData getMovieInformations(File file) {
         TechData techData = new TechData();
-        
+
         techData.setSize(file.length());
-         
+
+        if(isWindows())
+            NativeLibrary.addSearchPath("libvlc", "C:\\Program Files\\VideoLAN\\VLC"); // or whatever
         
-        NativeLibrary.addSearchPath("libvlc", "C:\\Program Files\\VideoLAN\\VLC"); // or whatever
         MediaPlayerFactory factory = new MediaPlayerFactory();
         MediaPlayer mediaPlayer = factory.newHeadlessMediaPlayer();
 
         mediaPlayer.prepareMedia(file.getPath());
-        
+
         mediaPlayer.parseMedia();
 
-       // logger.debug("Track Information after parse(): " + mediaPlayer.getTrackInfo());
-       // logger.debug("Track Information after parse(): " + mediaPlayer.getLength());
+        // logger.debug("Track Information after parse(): " + mediaPlayer.getTrackInfo());
+        // logger.debug("Track Information after parse(): " + mediaPlayer.getLength());
 
-        
-        
-        for(TrackInfo t : mediaPlayer.getTrackInfo()) {
-            if(t.getClass().getName().equals("uk.co.caprica.vlcj.player.VideoTrackInfo")) {
+
+
+        for (TrackInfo t : mediaPlayer.getTrackInfo()) {
+            if (t.getClass().getName().equals("uk.co.caprica.vlcj.player.VideoTrackInfo")) {
                 VideoTrackInfo v = (VideoTrackInfo) t;
-                logger.debug("Codec name: "+v.codecName());
-                logger.debug("height : "+v.height());
-                logger.debug("width : "+v.width());
                 techData.setCodecName(v.codecName());
                 techData.setHeight(v.height());
                 techData.setWidth(v.width());
             }
         }
-        
+
         logger.info(techData);
-        
+
 //        mediaPlayer.start();
 //
 //        try {
