3b990b0488ee369467ea1995136833e034c45925
Guillaume MANCIET <mudounet@free.fr>
2011-10-07 09:33:59 +0200

    Implemented all necessary libraries for snapshot.

---
+++ src/main/java/com/mudounet/hibernate/Image.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet.hibernate;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * @hibernate.class
+ * discriminator-value="I"
+ * @hibernate.discriminator
+ * column="TYPE"
+ * type="char"
+ **/
+public class Image {
+
+    private String path;
+    private String md5sum;
+    private long id;
+
+    public Image() {
+    }
+
+    /**
+     * @hibernate.id
+     * generator-class="native"
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @hibernate.property
+     */
+    public String getMd5sum() {
+        return md5sum;
+    }
+
+    public void setMd5sum(String md5sum) {
+        this.md5sum = md5sum;
+    }
+
+    /**
+     * @hibernate.property
+     */
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public BufferedImage load() {
+        BufferedImage img = null;
+
+        try {
+            img = ImageIO.read(new File("strawberry.jpg"));
+        } catch (IOException e) {
+        }
+        return img;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" + "path=" + path + '}';
+    }
+}
--- src/main/java/com/mudounet/hibernate/movies/others/Snapshot.java
+++ src/main/java/com/mudounet/hibernate/movies/others/Snapshot.java
@@ -4,88 +4,24 @@
  */
 package com.mudounet.hibernate.movies.others;
 
+import com.mudounet.hibernate.Image;
 
 /**
- * @hibernate.class
+ * @hibernate.subclass
+ * discriminator-value="M"
  **/
-public class Snapshot {
+public class Snapshot extends Image {
 
-    private Long id;
-    private long playTime = -1;
-    private int width = -1;
-    private int height = -1;
-    private String codecName = "";
-    private long size = -1;
+    private long time;
 
-    /**
-     * @hibernate.id
-     * generator-class="native"
-     */
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
+     /**
      * @hibernate.property
      */
-    public int getHeight() {
-        return height;
+    public long getTime() {
+        return time;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    /**
-     * @hibernate.property
-     */
-    public long getPlayTime() {
-        return playTime;
-    }
-
-    public void setPlayTime(long playTime) {
-        this.playTime = playTime;
-    }
-
-    /**
-     * @hibernate.property
-     */
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
-
-    /**
-     * @hibernate.property
-     */
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    /**
-     * @hibernate.property
-     */
-    public String getCodecName() {
-        return codecName;
-    }
-
-    public void setCodecName(String codecName) {
-        this.codecName = codecName;
-    }
-
-    @Override
-    public String toString() {
-        return "Snapshot{" + "id=" + id + ", playTime=" + playTime + ", width=" + width + ", height=" + height + ", codecName=" + codecName + ", size=" + size + '}';
+    public void setTime(long time) {
+        this.time = time;
     }
 }
