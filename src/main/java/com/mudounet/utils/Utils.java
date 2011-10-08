ac5fb918b5e6afadb649c0a68f76d74aa3dbc072
gmanciet <gmanciet@CNITC13LMN.dom2.ad.sys>
2011-10-09 00:18:57 +0200

    Added javadoc. Solved some warnings.

--- src/main/java/com/mudounet/utils/Utils.java
+++ src/main/java/com/mudounet/utils/Utils.java
@@ -42,7 +42,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.logging.Level;
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
