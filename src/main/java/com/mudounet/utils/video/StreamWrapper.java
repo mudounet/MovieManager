54c28b3d5ad2c8b265d6f91b95bdcd4b8df0c113
gmanciet <gmanciet@CNITC13LMN.dom2.ad.sys>
2011-10-09 00:23:48 +0200

    Added javadoc. Solved some warnings.

--- src/main/java/com/mudounet/utils/video/StreamWrapper.java
+++ src/main/java/com/mudounet/utils/video/StreamWrapper.java
@@ -1,20 +1,3 @@
-/* 
- * This file is part of Quelea, free projection software for churches.
- * Copyright (C) 2011 Michael Berry
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.mudounet.utils.video;
 
 import java.io.InputStream;
@@ -24,7 +7,7 @@ import java.io.OutputStream;
  * A helper class for wrapping input and output streams returned from remote
  * processes. These can used for a quick and dirty form of RMI between the Java
  * VMs.
- * @author Michael
+ * @author gmanciet
  */
 public class StreamWrapper {
 
--- src/main/java/com/mudounet/utils/video/VideoControlPanel.java
+++ src/main/java/com/mudounet/utils/video/VideoControlPanel.java
@@ -1,20 +1,3 @@
-/* 
- * This file is part of Quelea, free projection software for churches.
- * Copyright (C) 2011 Michael Berry
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.mudounet.utils.video;
 
 import com.mudounet.utils.Utils;
@@ -43,11 +26,12 @@ import org.pushingpixels.substance.internal.ui.SubstanceSliderUI;
 
 /**
  * The control panel for displaying the video.
- * @author Michael
+ * @author gmanciet
  */
 public class VideoControlPanel extends JPanel {
 
     private static final boolean TEST_DISABLE_SUBSTANCE = false;
+    private static final long serialVersionUID = 1L;
     private JButton play;
     private JButton pause;
     private JButton stop;
