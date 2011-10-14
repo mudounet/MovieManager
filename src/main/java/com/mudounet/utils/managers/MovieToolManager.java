c12410d6d9689ac61c37e3ab9c5ad82b31e93a29
Guillaume MANCIET <mudounet@free.fr>
2011-10-14 15:32:02 +0200

    Added comments

--- src/main/java/com/mudounet/utils/managers/MovieToolManager.java
+++ src/main/java/com/mudounet/utils/managers/MovieToolManager.java
@@ -34,37 +34,45 @@ public class MovieToolManager {
         return null;
     }
 
-    public static Set<Snapshot> genSnapshots(GenericMovie movie , File directory, int nbOfSnapshots) throws Exception {
+    /**
+     * Generate a set of snapshots from Movie.
+     * @param movie
+     * @param directory
+     * @param nbOfSnapshots
+     * @return found snapshots
+     * @throws Exception
+     */
+    public static Set<Snapshot> genSnapshots(GenericMovie movie, File directory, int nbOfSnapshots) throws Exception {
         Set<Snapshot> list = new HashSet<Snapshot>();
-        
+
         if(!directory.isDirectory()) {
             throw new Exception("Directory doesn't exists");
         }
         if(nbOfSnapshots < 1 || nbOfSnapshots > 9) {
             throw new Exception("Number of snapshots has to be defined between 1 and 9.");
         }
-        
-        String prefix = movie.getMd5()+"-";
-        
-        RemotePlayer headlessRemotePlayer = RemotePlayerFactory.getHeadlessRemotePlayer();
-        headlessRemotePlayer.load("src/test/resources/sample_video.flv");
-        headlessRemotePlayer.play();
 
-        long length = headlessRemotePlayer.getLength();
-        
-        for (long i = 1; i <= nbOfSnapshots; i++) {
-            Snapshot s = new Snapshot();
+        String prefix = movie.getMd5()+"-";
+
+                RemotePlayer headlessRemotePlayer = RemotePlayerFactory.getHeadlessRemotePlayer();
+                headlessRemotePlayer.load("src/test/resources/sample_video.flv");
+                headlessRemotePlayer.play();
+
+                long length = headlessRemotePlayer.getLength();
+
+                for (long i = 1; i <= nbOfSnapshots; i++) {
+                    Snapshot s = new Snapshot();
             s.setPath(directory.getAbsolutePath()+File.separator+prefix + i + ".png");
-            s.setTime(length * i / (nbOfSnapshots + 1));
+                    s.setTime(length * i / (nbOfSnapshots + 1));
 
             if(headlessRemotePlayer.takeSnapshot(s.getTime(), s.getPath())) {
-                list.add(s);
-            }
+                        list.add(s);
+                    }
             else {
                 logger.error(s.getPath()+" is not generated.");
+                }
             }
-        }
         
         return list;
     }
-}
+                }
