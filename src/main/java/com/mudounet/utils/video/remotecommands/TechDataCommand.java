b757bad96aefc6433f8804583935e8d1f8e88457
Guillaume MANCIET <mudounet@free.fr>
2012-03-26 15:29:34 +0200

    Added support for technical datas.

---
+++ src/main/java/com/mudounet/utils/video/remotecommands/TechDataCommand.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet.utils.video.remotecommands;
+
+/**
+ *
+ * @author gmanciet
+ */
+public class TechDataCommand extends com.mudounet.hibernate.movies.others.TechData {
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    public String toString() {
+        return "TechDataCommand{" + super.toString() + '}';
+    }
+}
