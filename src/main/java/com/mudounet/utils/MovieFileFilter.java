0cf755203656e49d70eb144b58636e457dbc48bf
Guillaume MANCIET <mudounet@free.fr>
2012-07-09 23:43:06 +0200

    Added file filter for movies

---
+++ src/main/java/com/mudounet/utils/MovieFileFilter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author isabelle
+ */
+public class MovieFileFilter implements FileFilter {
+
+    public boolean accept(File file) {
+        
+        String[] list = {"avi", "mp4", "mpg", "wmv", "mkv", "vob", "mov"};
+        List<String> listOfMovies = Arrays.asList(list);
+
+        for (String e : listOfMovies) {
+            if(file.getName().toLowerCase().endsWith(e)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
