bc77708c93c040aa7a5e7174901fae27090ed526
Guillaume MANCIET <mudounet@free.fr>
2012-07-09 23:43:31 +0200

    Added main application

---
+++ src/main/java/com/mudounet/App.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet;
+
+import com.mudounet.hibernate.Movie;
+import com.mudounet.utils.MovieFileFilter;
+import com.mudounet.utils.hibernate.AbstractDao;
+import com.mudounet.utils.hibernate.DataAccessLayerException;
+import com.mudounet.utils.hibernate.HibernateFactory;
+import com.mudounet.utils.managers.MovieListManager;
+import com.mudounet.utils.managers.SimpleTagManager;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author isabelle
+ */
+public class App {
+
+    protected static Logger logger = LoggerFactory.getLogger(App.class.getName());
+    public static AbstractDao template;
+
+    public static void main(String[] args) throws DataAccessLayerException {
+
+        System.out.println("Building Hibernate...");
+        HibernateFactory.buildSessionFactory();
+        template = new AbstractDao();
+
+        System.out.println("Building Movie list...");
+
+        List<File> listOfMovies = readDirWithMovies("/Volumes/Secondaire/jdownloader/Dw/liste");
+        
+        SimpleTagManager manager = new SimpleTagManager(template);
+        
+        List<Movie> movies = manager.getMovies();
+        
+        for (File e : listOfMovies) {
+            if(movies.contains(e)) {
+                 System.out.println("EXISTS: "+e.getName());
+            } 
+            else {
+                MovieListManager.addMovie(e.getAbsolutePath(), e.getName());
+                System.out.println("NEW: "+e.getName());
+            }
+        }
+    }
+
+    public static List<File> readDirWithMovies(String directory) {
+        File dir = new File(directory);
+        File[] listFiles = dir.listFiles(new MovieFileFilter());
+        List<File> listOfMovies = Arrays.asList(listFiles);
+        return listOfMovies;
+    }
+}
