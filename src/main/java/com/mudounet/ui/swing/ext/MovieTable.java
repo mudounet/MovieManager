f7048145571f753ed7c0bb5a04ff14d3051b1d6c
Guillaume MANCIET <mudounet@free.fr>
2012-07-24 20:34:16 +0200

    Changed interface to display movies.

--- src/main/java/com/mudounet/ui/swing/ext/MovieTable.java
+++ src/main/java/com/mudounet/ui/swing/ext/MovieTable.java
@@ -6,6 +6,7 @@ package com.mudounet.ui.swing.ext;
 
 import com.mudounet.hibernate.Movie;
 import com.mudounet.utils.Utils;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.List;
@@ -14,21 +15,26 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 /**
  *
  * @author isabelle
  */
 public class MovieTable extends JPanel {
-    private static final long serialVersionUID = 1L;
 
+    private static final long serialVersionUID = 1L;
 
     public MovieTable(List<Movie> listOfMovies) {
         super(new GridLayout(1, 0));
 
         final JTable table = new JTable(new MovieTableModel(listOfMovies));
+        table.getColumnModel().getColumn(1).setCellRenderer(new DurationRenderer());
+        table.getColumnModel().getColumn(3).setCellRenderer(new FileSizeRenderer());
         table.setPreferredScrollableViewportSize(new Dimension(500, 70));
         table.setFillsViewportHeight(true);
+        table.setAutoCreateRowSorter(true);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         //Create the scroll pane and add the table to it.
@@ -41,20 +47,20 @@ public class MovieTable extends JPanel {
     }
 
     class MovieTableModel extends AbstractTableModel {
-        private static final long serialVersionUID = 1L;
 
-        private String[] columnNames = {"Movie title", "duration", "Dimensions", "size",  "Type"};
+        private static final long serialVersionUID = 1L;
+        private String[] columnNames = {"Movie title", "Duration", "Dimensions", "Size", "Type"};
         private List<Movie> listOfMovies;
 
         public MovieTableModel(List<Movie> listOfMovies) {
             this.listOfMovies = listOfMovies;
         }
-        
+
         @Override
         public String getColumnName(int col) {
-        return columnNames[col].toString();
-    }
-        
+            return columnNames[col].toString();
+        }
+
         public int getRowCount() {
             return listOfMovies.size();
         }
@@ -64,19 +70,49 @@ public class MovieTable extends JPanel {
         }
 
         public Object getValueAt(int row, int col) {
-            if(this.listOfMovies == null) {
+            if (this.listOfMovies == null) {
                 return null;
             }
             Movie m = this.listOfMovies.get(row);
             switch (col) {
-            case 0:  return m.getTitle();
-            case 1:  return Utils.readableDuration(m.getMediaInfo().getPlayTime());
-            case 2:  return m.getMediaInfo().getVideoWidth()+" x "+m.getMediaInfo().getVideoHeight();
-            case 3:  return Utils.readableFileSize(m.getSize());
-            case 4:  return m.getMediaInfo().getVideoCodec().toUpperCase();
-        }
-            
+                case 0:
+                    return m.getTitle();
+                case 1:
+                    return m.getMediaInfo().getPlayTime();
+                case 2:
+                    return m.getMediaInfo().getVideoWidth() + " x " + m.getMediaInfo().getVideoHeight();
+                case 3:
+                    return m.getSize();
+                case 4:
+                    return m.getMediaInfo().getVideoCodec().toUpperCase();
+            }
+
             return null;
         }
     }
+
+    class DurationRenderer extends DefaultTableCellRenderer {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof Long) {
+                this.setText(Utils.readableDuration(Long.parseLong(value.toString())));
+            }
+        }
+    }
+    
+        class FileSizeRenderer extends DefaultTableCellRenderer {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof Long) {
+                this.setText(Utils.readableFileSize(Long.parseLong(value.toString())));
+            }
+        }
+    }
+    
 }
---
+++ src/main/java/com/mudounet/ui/swing/ext/TagTable.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mudounet.ui.swing.ext;
+
+import com.mudounet.ui.swing.ext.*;
+import com.mudounet.hibernate.Movie;
+import com.mudounet.utils.Utils;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author isabelle
+ */
+public class TagTable extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+
+    public TagTable(List<Movie> listOfMovies) {
+        super(new GridLayout(1, 0));
+
+        final JTable table = new JTable(new MovieTableModel(listOfMovies));
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+
+
+
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+    }
+
+    class MovieTableModel extends AbstractTableModel {
+        private static final long serialVersionUID = 1L;
+
+        private String[] columnNames = {"Movie title", "duration", "Dimensions", "size",  "Type"};
+        private List<Movie> listOfMovies;
+
+        public MovieTableModel(List<Movie> listOfMovies) {
+            this.listOfMovies = listOfMovies;
+        }
+        
+        @Override
+        public String getColumnName(int col) {
+        return columnNames[col].toString();
+    }
+        
+        public int getRowCount() {
+            return listOfMovies.size();
+        }
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public Object getValueAt(int row, int col) {
+            if(this.listOfMovies == null) {
+                return null;
+            }
+            Movie m = this.listOfMovies.get(row);
+            switch (col) {
+            case 0:  return m.getTitle();
+            case 1:  return Utils.readableDuration(m.getMediaInfo().getPlayTime());
+            case 2:  return m.getMediaInfo().getVideoWidth()+" x "+m.getMediaInfo().getVideoHeight();
+            case 3:  return Utils.readableFileSize(m.getSize());
+            case 4:  return m.getMediaInfo().getVideoCodec().toUpperCase();
+        }
+            
+            return null;
+        }
+    }
+}
