54c625a881f0fd5c8c81e3ea222ed38a2d571177
Guillaume MANCIET <mudounet@free.fr>
2012-07-29 22:24:17 +0200

    Added handling of hibernate tags

--- src/main/java/com/mudounet/TagManager.java
+++ src/main/java/com/mudounet/TagManager.java
@@ -1,13 +1,18 @@
 package com.mudounet;
 
-import java.io.Console;
+import com.mudounet.hibernate.tags.Tag;
+import com.mudounet.utils.hibernate.DataAccessLayerException;
+import com.mudounet.utils.hibernate.HibernateThreadSession;
 import javax.swing.JOptionPane;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author isabelle
  */
 public class TagManager {
+
+    protected static org.slf4j.Logger logger = LoggerFactory.getLogger(TagManager.class.getName());
 
     public static void main(String[] args) {
         String tagRef;
@@ -22,6 +27,28 @@ public class TagManager {
                     possibilities,
                     "");
 
+            if ((tagRef != null) && (tagRef.length() > 0)) {
+                addTag(tagRef);
+            }
+
         } while (((tagRef != null) && (tagRef.length() > 0)));
+    }
+
+    public static void addTag(String tagToAdd) {
+        try {
+            HibernateThreadSession template = GlobalProperties.getTemplate();
+
+            Tag t = new Tag();
+            t.setKey(tagToAdd);
+            if(template.find(Tag.class, "key", tagToAdd) != null) {
+                logger.error("Tag "+tagToAdd+" is already defined");
+                return;
+            }
+            template.beginTransaction();
+            template.saveOrUpdate(t);
+            template.endTransaction();
+        } catch (DataAccessLayerException ex) {
+            logger.error(ex.getMessage());
+        }
     }
 }
